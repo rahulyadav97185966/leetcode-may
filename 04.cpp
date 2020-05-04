f9924f4d16504fdaf468f0b7586ad34bcd12f207
sagar yadav <40927722+rahulyadav97185966@users.noreply.github.com>
2020-05-04 14:07:40 +0530

    Create 04.cpp

---
+++ 04.cpp
@@ -0,0 +1,14 @@
+/Number Complement
+class Solution {
+public:
+    int findComplement(int num) {
+        int count=0;
+        int j=num;
+        while(j!=0){
+            j=j/2;
+            count++;
+        }
+        int ans = pow(2,count) - 1;
+        return num ^ ans;
+    }
+};
