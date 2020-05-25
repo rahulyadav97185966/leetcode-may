217da5cfb499176311be849ba0b343baaeb8059f
sagar yadav <40927722+rahulyadav97185966@users.noreply.github.com>
2020-05-25 17:42:07 +0530

    Create 25.cpp

---
+++ 25.cpp
@@ -0,0 +1,15 @@
+//Uncrossed Lines
+//this is same as finding the longest common subsequence between teo strings.
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& A, vector<int>& B) {
+        vector<vector<int>>dp(A.size()+1,vector<int>(B.size()+1,0));
+        for(int i=1 ; i<=A.size() ; i++)
+            for(int j=1 ; j<=B.size() ; j++)
+            {
+                if(A[i-1]==B[j-1]) dp[i][j]=dp[i-1][j-1]+1;
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        return dp[A.size()][B.size()];
+    }
+};
